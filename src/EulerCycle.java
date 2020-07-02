6c2506a967c3c478905dc6ac3140f70400f34755
amichai-H <51719573+amichai-H@users.noreply.github.com>
2020-07-02 20:53:43 +0300

    fix

--- src/EulerCycle.java
+++ src/EulerCycle.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class EulerCycle {
@@ -5,42 +6,46 @@ public class EulerCycle {
     public EulerCycle(Graph g){
         this.graph = new Graph(g);
     }
-    public Stack findCycle(){
+    public ArrayList<Integer> findCycle(){
         if (!isEuler()){
             return null;
         }
-        Stack<Integer> s = new Stack<>() , c = new Stack<>();
+        Stack<Integer> s = new Stack<>();
+        ArrayList<Integer> arrayList = new ArrayList();
         s.push(1);
         while (!s.isEmpty()){
             int u = s.peek();
             if (graph.deg(u) == 0){
                 s.pop();
-                c.push(u);
+                arrayList.add(u);
             }
             else {
-                int v = graph.getEdges(u).iterator().next().getDst();
+                int v = graph.getEdges(u).iterator().next().getDst();//
                 s.push(v);
                 graph.removeEdge(u,v);
             }
         }
-        return c;
+        return arrayList;
     }
     private boolean isEuler() {
         return true;
     }
     public static void main(String[] args){
         Graph g = new Graph();
-        for (int i = 0;i<5;i++){
+        for (int i = 0;i<6;i++){
             g.addNode();
         }
         g.add2Edge(1,3);
+        g.add2Edge(1,6);
         g.add2Edge(1,2);
+        g.add2Edge(6,2);
         g.add2Edge(1,5);
         g.add2Edge(2,3);
         g.add2Edge(2,5);
         g.add2Edge(3,5);
         g.add2Edge(3,4);
         g.add2Edge(5,4);
-        System.out.println("Euler Circle: " + new EulerCycle(g).findCycle().toString()); // [2, 5, 4, 3, 5, 1, 2, 3, 1]
+        System.out.println("Euler Circle: " + new EulerCycle(g).findCycle().toString()); // Euler Circle: [1, 5, 4, 3, 5, 2, 6, 1, 2, 3, 1]
     }
 }
+
